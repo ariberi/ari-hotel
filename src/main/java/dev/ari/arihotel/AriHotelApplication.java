e2003650bd0aa703ff2708be9048cf12a9a96eec
Ariel <berinsteinariel@gmail.com>
2025-04-30 12:47:21 -0300

    first commit

---
+++ src/main/java/dev/ari/arihotel/AriHotelApplication.java
@@ -0,0 +1,13 @@
+package dev.ari.arihotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AriHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ari/arihotel/AriHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ari.arihotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
