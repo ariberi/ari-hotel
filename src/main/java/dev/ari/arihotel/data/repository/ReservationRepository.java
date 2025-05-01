30f2150aaa9c49adf1d0c95740cfa770174d6f15
Ariel <berinsteinariel@gmail.com>
2025-05-01 17:24:38 -0300

    create room reservation service

--- src/main/java/dev/ari/arihotel/data/repository/ReservationRepository.java
+++ src/main/java/dev/ari/arihotel/data/repository/ReservationRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.sql.Date;
 import java.util.List;
-import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
